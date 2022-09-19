80be5756f50ca5a8140d5f93dac0ecb947e0086b
DaniyalGuru <danisamim9@gmail.com>
2022-09-19 23:34:47 +0500

    'update'

--- app.js
+++ app.js
@@ -19,6 +19,6 @@ connectDB();
 // index Routes
 app.use("/", require("./routers/index"));
 
-app.listen(3000, () => {
+app.listen(process.env.PORT | 3000, () => {
   console.log("Server is up and running");
 });
